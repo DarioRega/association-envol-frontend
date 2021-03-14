2e25b165ef831714844d955902b85aa02b78dcf6
dario <dario.regazzoni@outlook.fr>
2021-03-14 21:42:05 +0100

    Soutenir envol layout

---
+++ layouts/helpEnvol.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <container>
+      <div class="flex justify-end pt-12">
+        <nuxt-link
+          to="/"
+          class="rounded-full text-4xl font-normal tracking-wider text-white bg-brand-dark-blue px-10 py-3 font-heading hover:bg-white hover:text-brand-dark-blue border-2 border-brand-dark-blue transition-colors duration-300"
+          >{{ $t('helpEnvol.backToHome') }}</nuxt-link
+        >
+      </div>
+    </container>
+    <Nuxt class="flex-grow" />
+  </div>
+</template>
+<script>
+import Container from '@/components/containers/Container';
+export default {
+  components: { Container },
+};
+</script>
---
+++ pages/soutenir-envol.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <container> hello soutenir </container>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'helpEnvol',
+  name: 'SoutenirEnvol',
+};
+</script>
+
+<style scoped></style>
